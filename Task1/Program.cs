ead66341e3ad81815451c757a67685df3f305dab
Igor Ostrohliad <sana22_oid@student.ztu.edu.ua>
2022-11-08 18:02:55 +0200

    Change three line

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("1st line");
+Console.WriteLine("1st line");
+Console.WriteLine("1st line");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
